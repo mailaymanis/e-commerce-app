85446f1e5e5ffbea027619bcf827ad732b5caed7
ayman elkafrawy <120125370+mailaymanis@users.noreply.github.com>
2024-12-23 20:00:30 +0200

    create category screen and show all app categories on it
    Signed-off-by: ayman elkafrawy <120125370+mailaymanis@users.noreply.github.com>

--- lib/features/category/presentation/view/screen/categories_screen.dart
+++ lib/features/category/presentation/view/screen/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/features/layout/presentation/view_model/cubit/layout_cubit.dart';
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
   @override
@@ -8,9 +10,35 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body:Center(
-        child:Text("Category Items"),
+    final cubit = BlocProvider.of<LayoutCubit>(context).categories;
+    return  Scaffold(
+      body:SafeArea(
+        child:Padding(
+          padding:EdgeInsets.symmetric(vertical:10 , horizontal:20),
+          child:GridView.builder(
+              gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount:2,
+                crossAxisSpacing:15,
+                mainAxisSpacing:15,
+                childAspectRatio:0.8,
+              ),
+              itemCount:cubit.length,
+              itemBuilder:(context , index){
+                return Container(
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Expanded(child:Image.network(cubit[index].image!)),
+                      const SizedBox(height: 10,),
+                      Text(cubit[index].name! , style:const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    ],
+                  ),
+                );
+              }
+          ),
+        ),
       ),
     );
   }
